c24e763945396d263ed17d4ba066139e9ed19e82
Neo Ho <neo.ho@johnsontechinc.com>
2018-06-04 20:14:44 +0800

    [feat] SendGrid POC, including basic authentication and SendGrid api key.

---
+++ SendMailThrougSendGrid/MailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SendMailThrougSendGrid
+{
+    internal class MailSender
+    {
+        private static async Task Main()
+        {
+            var msg = new SendGridMessage();
+
+            msg.SetFrom(new EmailAddress("neofelisho@gmail.com", "Neo Ho"));
+
+            var recipients = new List<EmailAddress>
+            {
+                new EmailAddress("neofelisho@gmail", "Neo Ho"),
+                //new EmailAddress("anna@example.com", "Anna Lidman"),
+                //new EmailAddress("peter@example.com", "Peter Saddow")
+            };
+            msg.AddTos(recipients);
+
+            msg.SetSubject("Message from Neo Ho through SendGrid");
+
+            msg.AddContent(MimeType.Text, "Hello World plain text!");
+            msg.AddContent(MimeType.Html, "<p>Hello World!</p>");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory, string.Format("..{0}..{0}..{0}", Path.DirectorySeparatorChar)))
+                .AddJsonFile("appsettings.json");
+            var apiKey = builder.Build().GetSection("SENDGRID_API_KEY").Value;
+
+            var client = new SendGridClient(apiKey);
+            var response = await client.SendEmailAsync(msg);
+
+            Console.WriteLine(response.StatusCode);
+            Console.Read();
+        }
+    }
+}
---
+++ SendNetMailThroughSendGrid/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace SendNetMailThroughSendGrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var mailMsg = new MailMessage();
+
+                // To
+                mailMsg.To.Add(new MailAddress("neofelisho@gmail.com", "Neo Ho"));
+
+                // From
+                mailMsg.From = new MailAddress("neofelisho@gmail.com", "Neo Ho");
+
+                // Subject and multipart/alternative Body
+                mailMsg.Subject = "Message from Neo Ho through SendGrid";
+                const string text = "Hello World plain text!";
+                const string html = @"<p>Hello World!</p>";
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+                // Init SmtpClient and send
+                var smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                var credentials = new System.Net.NetworkCredential("azure_user_name@azure.com", "sendgrid_password");
+                smtpClient.Credentials = credentials;
+
+                smtpClient.Send(mailMsg);
+                Console.WriteLine("Success.");
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
